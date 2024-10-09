d4a84ed6cc798f9ec956c18d6b158ca72e436d02
Olzhas <olzhas.taukent@gmail.com>
2024-10-09 12:25:16 +0600

    updated quiz app

--- coursePlatformBack/course_platform_back/quiz/models.py
+++ coursePlatformBack/course_platform_back/quiz/models.py
@@ -3,7 +3,22 @@ from django.db import models
 class Question(models.Model):
     text = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.text
+
+
 class Answer(models.Model):
-    question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
-    text = models.CharField(max_length=255)
-    is_correct = models.BooleanField(default=False)
+    question = models.OneToOneField(Question, on_delete=models.CASCADE, related_name='answer')
+    option_1 = models.CharField(max_length=255)
+    option_2 = models.CharField(max_length=255)
+    option_3 = models.CharField(max_length=255)
+    option_4 = models.CharField(max_length=255)
+    correct_option = models.IntegerField(choices=[
+        (1, 'Option 1'),
+        (2, 'Option 2'),
+        (3, 'Option 3'),
+        (4, 'Option 4'),
+    ])  # Ссылается на правильный вариант (1, 2, 3 или 4)
+
+    def __str__(self):
+        return f"Answer for: {self.question.text}"
--- coursePlatformBack/course_platform_back/quiz/views.py
+++ coursePlatformBack/course_platform_back/quiz/views.py
@@ -1,27 +1,52 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Question, Answer
+from .models import Question
+import json
 
 
+# Обработка GET-запроса для получения вопросов и ответов
 @api_view(['GET'])
 def get_test(request):
-    questions = Question.objects.prefetch_related('answers').all()
+    questions = Question.objects.all()
     data = []
+
     for question in questions:
-        answers = question.answers.all()
+        answer = question.answer
         data.append({
             'id': question.id,
             'text': question.text,
-            'answers': [{'id': answer.id, 'text': answer.text} for answer in answers]
+            'answer': {
+                'option_1': answer.option_1,
+                'option_2': answer.option_2,
+                'option_3': answer.option_3,
+                'option_4': answer.option_4,
+                'correct_option': answer.correct_option,
+            }
         })
+
     return Response(data)
 
+
+# Обработка POST-запроса для отправки теста и расчета результата
 @api_view(['POST'])
 def submit_test(request):
-    user_answers = request.data.get('answers', {})
-    score = 0
-    for question_id, answer_id in user_answers.items():
-        correct_answer = Answer.objects.filter(question_id=question_id, is_correct=True).first()
-        if correct_answer and correct_answer.id == int(answer_id):
-            score += 1
-    return Response({'score': score})
+    try:
+        data = request.data  # Извлечение данных POST-запроса
+        score = 0
+
+        for question_id, selected_answer in data['answers'].items():
+            question = Question.objects.get(id=question_id)
+            correct_answer = question.answer.correct_option
+            if int(selected_answer) == correct_answer:
+                score += 1
+
+        return Response({'score': score})
+
+    except Question.DoesNotExist:
+        return Response({'error': 'Question not found'}, status=404)
+
+    except KeyError:
+        return Response({'error': 'Invalid data format'}, status=400)
+
+    except Exception as e:
+        return Response({'error': str(e)}, status=500)
