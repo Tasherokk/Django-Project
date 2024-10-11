27d916e78dcf600b5e4181c160b29dbb646b8e01
User <bekarys2004r@gmail.com>
2024-10-11 18:18:27 +0500

    divide backend

--- coursePlatformBack/course_platform_back/video/views.py
+++ coursePlatformBack/course_platform_back/video/views.py
@@ -1,22 +1,25 @@
-from django.http import StreamingHttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse, StreamingHttpResponse
+from django.shortcuts import get_object_or_404
 from .models import Video
 from .services import open_file
 
-# Create your views here.
 def get_list_video(request):
-    return render(request, 'videoTemplates/home.html', {'video_list': Video.objects.all()})
-
+    videos = Video.objects.all().values('id', 'title', 'description', 'image', 'create_at')
+    return JsonResponse(list(videos), safe=False)
 
 def get_video(request, pk: int):
     _video = get_object_or_404(Video, id=pk)
-    return render(request, "videoTemplates/video.html", {"video": _video})
-
+    return JsonResponse({
+        "id": _video.id,
+        "title": _video.title,
+        "description": _video.description,
+        "image": request.build_absolute_uri(_video.image.url),
+        "create_at": _video.create_at,
+    })
 
 def get_streaming_video(request, pk: int):
     file, status_code, content_length, content_range = open_file(request, pk)
     response = StreamingHttpResponse(file, status=status_code, content_type='video/mp4')
-
     response['Accept-Ranges'] = 'bytes'
     response['Content-Length'] = str(content_length)
     response['Cache-Control'] = 'no-cache'
